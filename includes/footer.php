5a0c21164d4199436bd04d065e1e652d62bff38e
PHP Manhã <php-m@itapevi.local>
2025-06-04 10:14:14 -0300

    Catálogo em PHP

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+</main>
+    <footer>
+        <p>&copy; 2025. Todos os direitos reservados.</p>
+        <p>Desenvolvido por Brayan</p>
+    </footer>
+</body>
+
+</html>
---
+++ includes/produtos.php
@@ -0,0 +1,87 @@
+<?php
+$produtos = [
+    [
+    'nome' => 'BMW G 310R',
+    'foto' => '1.jpg',
+    'desc' => 'A BMW G 310 R é a companheira perfeita para quem busca liberdade sobre duas rodas com estilo e confiança..',
+    'preco' => 35588,
+    'cat' => 'Trail e Naked'
+    ],    
+    [
+    'nome' => 'BMW R 1200 GS',
+    'foto' => '2.jpg',
+    'desc' => 'A BMW GS 1200 é uma moto aventureira, potente e confortável, ideal para longas viagens e terrenos variados.',
+    'preco' => 95555,
+    'cat' => 'Big Trails e Touring Premium'
+    ], 
+    [
+    'nome' => 'BMW 1600 GTL',
+    'foto' => '3.webp',
+    'desc' => 'A BMW K 1600 GTL é uma touring de luxo, potente e confortável, ideal para viagens longas.s',
+    'preco' => 298196,
+    'cat' => 'Big Trails e Touring Premium'
+    ],
+    [
+    'nome' => 'Ducati multistrada',
+    'foto' => '4.jpg',
+    'desc' => 'A Ducati Multistrada é perfeita para quem ama explorar novos caminhos, oferecendo potência e conforto em cada viagem',
+    'preco' => 93324,
+    'cat' => 'Trail e Naked'
+    ],
+    [
+    'nome' => 'BMW f800',
+    'foto' => '5.webp',
+    'desc' => 'A BMW F 800 GS é uma moto aventureira, ideal para quem adora explorar diferentes terrenos com conforto e agilidade. ',
+    'preco' => 61321,
+    'cat' => 'Trail e Naked'
+    ],
+    [
+    'nome' => 'Honda cg 160',
+    'foto' => '6.jpeg',
+    'desc' => 'A Honda CG 160 2022 é a companheira ideal para o dia a dia, trazendo economia e confiança em cada quilômetro.',
+    'preco' => 11999,
+    'cat' => 'Urbanas'
+    ],
+    [
+    'nome' => 'Honda xre 300',
+    'foto' => '7.jpg',
+    'desc' => 'A Honda XRE 300 2022 é a moto perfeita para quem busca aventuras com confiança, seja na cidade ou em trilhas.',
+    'preco' => 27324,
+    'cat' => 'Urbanas'
+    ],
+    [
+    'nome' => 'Honda nc 750',
+    'foto' => '8.webp',
+    'desc' => 'A Honda NC 750X 2024 é a moto perfeita para quem busca conforto e desempenho tanto nas viagens longas quanto no dia a dia.',
+    'preco' => 58687,
+    'cat' => 'Trail e Naked'
+    ],
+    [
+    'nome' => 'Royal enfield 350',
+    'foto' => '9.webp',
+    'desc' => 'A Royal Enfield Classic 350 2022 une o charme clássico com o prazer de uma pilotagem confortável e moderna.',
+    'preco' => 22299,
+    'cat' => 'Urbanas'
+    ],
+    [
+        'nome' => 'Triumph tiger 1200',
+    'foto' => '10.jpeg',
+    'desc' => 'A Tiger 1200 é uma verdadeira máquina, combinando potência impressionante com conforto excepcional.',
+    'preco' => 93990,
+    'cat' => 'Big Trails e Touring Premium'
+    ],
+    [
+        'nome' => 'Yamaha tenere 660',
+    'foto' => '11.jpg',
+    'desc' => 'A Ténéré 660 é uma moto versátil, projetada para aventuras off-road, com desempenho equilibrado e conforto tanto em terrenos difíceis quanto em estradas pavimentadas.',
+    'preco' => 39989,
+    'cat' => 'Trail e Naked'
+    ],
+    [
+    'nome' => 'Yamaha xt 660',
+    'foto' => '12.webp',
+    'desc' => 'A XT 660 é uma moto robusta e versátil, ideal para aventuras off-road, combinando potência, conforto e resistência em diversos tipos de terreno.',
+    'preco' => 45785,
+    'cat' => 'Urbanas'
+    ]
+    ];
